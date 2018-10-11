95261d81c8a5b4628afa2b8aea3fb9c94cc9e253
fwx426328 <1053800926@qq.com>
2018-10-11 22:17:05 +0800

    合并文件php
    Signed-off-by: fwx426328 <1053800926@qq.com>

---
+++ php/merge.php
@@ -0,0 +1,14 @@
+<?php 
+
+$hostdir= iconv("utf-8","gbk", getcwd().'\\temp\\') ;
+$filesnames = scandir($hostdir); 
+// var_dump($filesnames);
+foreach ($filesnames as $name) {
+	$cipath = $hostdir.$name;
+	var_dump($cipath);
+	$str = file_get_contents($cipath);
+	file_put_contents(getcwd().'\\temp\\111.zip',$str,FILE_APPEND);
+}
+
+
+ ?>
