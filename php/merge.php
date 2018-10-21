7c27d3f9fab59613cdd422011dc2a053842118c0
chinesexiaozhu <zhushengyou2017@sina.com>
2018-10-21 22:53:02 +0800

    'update'

--- php/merge.php
+++ php/merge.php
@@ -2,13 +2,14 @@
 
 $hostdir= iconv("utf-8","gbk", getcwd().'\\temp\\') ;
 $filesnames = scandir($hostdir); 
-// var_dump($filesnames);
+ var_dump($filesnames);
+
+ exit;
+
+
 foreach ($filesnames as $name) {
 	$cipath = $hostdir.$name;
 	var_dump($cipath);
 	$str = file_get_contents($cipath);
 	file_put_contents(getcwd().'\\temp\\111.zip',$str,FILE_APPEND);
 }
-
-
- ?>
--- php/upload.php
+++ php/upload.php
@@ -1,14 +1,14 @@
-<?php 
-if ($_FILES["file"]["error"] > 0)
-{
-    echo "错误：" . $_FILES["file"]["error"] . "<br>";
-}
-else
-{
-	$filename = $_POST["filename"];
-	move_uploaded_file(
-		$_FILES['file']['tmp_name'],
-	 	getcwd()."/temp/".$filename
-	);
-}
- ?>
+<?php
+    header("Content-type:text/html;charset=utf-8");
+    if ($_FILES["file"]["error"] > 0) {
+        echo "错误：" . $_FILES["file"]["error"] . "<br>";
+    } else {
+//        var_dump($_FILES);exit;
+        $filename = $_POST["filename"];
+        $FileDir = dirname(__FILE__)."/temp";
+        if (!is_dir($FileDir)) mkdir($FileDir,0777);
+        $isSuccess = move_uploaded_file($_FILES['file']['tmp_name'], $FileDir.DIRECTORY_SEPARATOR.$filename);
+        if (!$isSuccess) {
+            echo "file upload  failed";
+        }
+    }
