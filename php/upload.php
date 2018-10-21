d1a7463e5c60961702e392333d5435e68d6dab01
chinesexiaozhu <zhushengyou2017@sina.com>
2018-10-21 23:37:51 +0800

    update php file

--- php/upload.php
+++ php/upload.php
@@ -3,12 +3,17 @@
     if ($_FILES["file"]["error"] > 0) {
         echo "错误：" . $_FILES["file"]["error"] . "<br>";
     } else {
-//        var_dump($_FILES);exit;
+
         $filename = $_POST["filename"];
+        $pattren =  '/[\x{4e00}-\x{9fa5}]/u';
+        if (preg_match($pattren,$filename)) { //文件名存在中文字符不让提交;
+            exit('存在中文字符，不合法的文件名');
+        }
         $FileDir = dirname(__FILE__)."/temp";
         if (!is_dir($FileDir)) mkdir($FileDir,0777);
         $isSuccess = move_uploaded_file($_FILES['file']['tmp_name'], $FileDir.DIRECTORY_SEPARATOR.$filename);
         if (!$isSuccess) {
-            echo "file upload  failed";
+            exit("文件上传失败");
         }
+
     }
