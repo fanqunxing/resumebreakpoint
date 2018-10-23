67c7a064f1cfc04aa9a59ab8bdaaa437a70563c3
fwx426328 <1053800926@qq.com>
2018-10-23 22:50:02 +0800

    格式化代码
    Signed-off-by: fwx426328 <1053800926@qq.com>

--- js/fileupload.js
+++ js/fileupload.js
@@ -1,164 +1,121 @@
 /**
-  * Fileupload v1.0.0
-  * (c) fwx426328
-  */
+ * Fileupload v1.0.0
+ * (c) fwx426328
+ */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  typeof define === 'function' && define.cmd ? define(factory) :
-  (global.Fileupload = factory());
-})(this, function () { 'use strict';
+    typeof define === 'function' && define.amd ? define(factory) :
+    typeof define === 'function' && define.cmd ? define(factory) :
+    (global.Fileupload = factory());
+})(this, function () {
+  'use strict';
 
-var version = '1.0.0';
+  var version = '1.0.0';
 
-var _fileSign_ = null;
+  var _fileSign_ = null;
 
-function isDef(v) {
-  return v !== undefined && v !== null;
-};
-
-function isTrue(v) {
-  return v === true;
-};
-
-function isObject (obj) {
-  return obj !== null && typeof obj === 'object';
-};
-
-function isFunction(it) {
-  return Object.prototype.toString.call(it) === '[object Function]';
-};
-
-function isString (obj){
-  return Object.prototype.toString.call(obj) === '[object String]';
-}
-
-function noop() {};
-
-function slice (it) {
-  return [].slice.call(it);
-};
-
-function assert (condition, message) {
-  if (!condition) {
-    throw new Error(("Fileupload.js: " + message));
-  }
-};
-
-/**
- * 简单Promise
- */
-function Promise (fn) {
-  this.status = 'pending';
-  var _success = function () {};
-  var _error = function () {};
-  var _value = null;
-  var _self = this;
-  function resolve(value) {
-    _self.status = 'fulfilled';
-    _value = value;
-    setTimeout(function() {
-      _success(value);
-    }, 0);
+  function isDef(v) {
+    return v !== undefined && v !== null;
   };
-  function reject(value) {
-    _self.status = 'rejected';
-    _value = value;
-    setTimeout(function() {
-      _error(value);
-    }, 0);
-  };
-  this.then = function (success) {
-    if (_self.status == 'pending') {
-      _success = success;
-    } else {
-      success(_value);
-    };
-    return _self;
-  };
-  this.catch = function (error) {
-    if (_self.status == 'pending') {
-      _error = error;
-    } else {
-      error(_value);
-    };
-    return _self;
-  }; 
-  fn(resolve, reject);
-}
 
-function getMd5 (file) {
-  return new Promise(function (resolve, reject) {
+  function isTrue(v) {
+    return v === true;
+  };
+
+  function isObject(obj) {
+    return obj !== null && typeof obj === 'object';
+  };
+
+  function isFunction(it) {
+    return Object.prototype.toString.call(it) === '[object Function]';
+  };
+
+  function isString(obj) {
+    return Object.prototype.toString.call(obj) === '[object String]';
+  };
+
+  function noop() {};
+
+  function slice(it) {
+    return [].slice.call(it);
+  };
+
+  function assert(condition, message) {
+    if (!condition) {
+      throw new Error(("Fileupload.js: " + message));
+    }
+  };
+
+  function getMd5(file, fn) {
     var fileReader = new FileReader();
     try {
       fileReader.readAsArrayBuffer(file);
     } catch (e) {
-      reject(e);
+      fn(e);
     };
     fileReader.onload = function (e) {
       var md5 = _fileSign_(e.target.result);
-      resolve(md5);
+      fn(md5);
     };
-  });
-};
-
-function fileSlice (file, size) {
-  var fileList = [];
-  for (var i = 0, len = file.size / size; i < len; i++) {
-    var start = i * size;
-    var end = (i + 1) * size;
-    var blob = file.slice(start, end);
-    fileList.push({
-      index: i,
-      blob: blob
-    });
   };
-  return fileList;
-};
 
-function fileListSlice (fileList, size) {
-  var fileSliceList = [];
-  fileList.forEach(function(file) {
-    var filebloblist = fileSlice(file, size);
-    fileSliceList.push(filebloblist);
-  });
-  return fileSliceList;
-};
+  function fileSlice(file, size) {
+    var fileList = [];
+    for (var i = 0, len = file.size / size; i < len; i++) {
+      var start = i * size;
+      var end = (i + 1) * size;
+      var blob = file.slice(start, end);
+      fileList.push({
+        index: i,
+        blob: blob
+      });
+    };
+    return fileList;
+  };
 
-function hasProp(obj, prop) {
-  return Object.prototype.hasOwnProperty.call(obj, prop);
-};
+  function fileListSlice(fileList, size) {
+    var fileSliceList = [];
+    fileList.forEach(function (file) {
+      var filebloblist = fileSlice(file, size);
+      fileSliceList.push(filebloblist);
+    });
+    return fileSliceList;
+  };
 
-function eachProp(obj, func) {
-  var prop;
-  for (prop in obj) {
-    if (hasProp(obj, prop)) {
-      if (func(obj[prop], prop)) {
-        break;
+  function hasProp(obj, prop) {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+  };
+
+  function eachProp(obj, func) {
+    var prop;
+    for (prop in obj) {
+      if (hasProp(obj, prop)) {
+        if (func(obj[prop], prop)) {
+          break;
+        }
       }
     }
-  }
-};
-
-function mixin(target, source, force) {
-  if (source) {
-    eachProp(source, function (val, prop) {
-      if (isTrue(force) || !hasProp(target, prop)) {
-        target[prop] = val;
-      };
-    });
   };
-  return target;
-};
 
-var ajaxSetting = {
-  type: 'GET',
-  dataType: 'json',
-  data: {},
-  processData: true
-};
+  function mixin(target, source, force) {
+    if (source) {
+      eachProp(source, function (val, prop) {
+        if (isTrue(force) || !hasProp(target, prop)) {
+          target[prop] = val;
+        };
+      });
+    };
+    return target;
+  };
 
-function ajax(options) {
+  var ajaxSetting = {
+    type: 'GET',
+    dataType: 'json',
+    data: {},
+    processData: true
+  };
+
+  function ajax(options) {
     options = mixin(options, ajaxSetting);
     var params = options.data;
     if (options.processData) {
@@ -166,212 +123,206 @@ function ajax(options) {
     }
     var xhr = null;
     if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
+      xhr = new XMLHttpRequest();
     } else {
-        xhr = new ActiveXObject('Microsoft.XMLHTTP');
+      xhr = new ActiveXObject('Microsoft.XMLHTTP');
     }
     xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            var status = xhr.status;
-            if (status >= 200 && status < 300) {
-                options.success && options.success(xhr.responseText, xhr.responseXML);
-            } else {
-                options.fail && options.fail(status);
-            }
+      if (xhr.readyState == 4) {
+        var status = xhr.status;
+        if (status >= 200 && status < 300) {
+          options.success && options.success(xhr.responseText, xhr.responseXML);
+        } else {
+          options.error && options.error(status);
         }
+      }
     }
 
     if (options.type == "GET") {
-        xhr.open("GET", options.url + "?" + params, true);
-        xhr.send(null);
+      xhr.open("GET", options.url + "?" + params, true);
+      xhr.send(null);
     } else if (options.type == "POST") {
-        xhr.open("POST", options.url, true);
-        if (options.processData) {
-          xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        }
-        xhr.send(params);
+      xhr.open("POST", options.url, true);
+      if (options.processData) {
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      }
+      xhr.send(params);
     }
-}
+  };
 
-//格式化参数
-function formatParams(data) {
+  //格式化参数
+  function formatParams(data) {
     var arr = [];
     for (var name in data) {
-        arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
+      arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(data[name]));
     }
-    arr.push(("v=" + Math.random()).replace(".",""));
+    arr.push(("v=" + Math.random()).replace(".", ""));
     return arr.join("&");
-}
-
-
-
-Fileupload.fileSign = function(fileSign) {
-  _fileSign_ = fileSign;
-};
-
-function Fileupload () {
-  
-  var _fileList = [];
-
-  var _currentSliceList = [];
-
-  var _uploadFileUrl = '';
-
-  var _queryFileUrl = '';
-
-  var _mergeFileUrl = '';
-
-  var _sparkmd5 = '';
-
-  var _threadId = 0;
-
-  var _currentFile = '';
-
-  var _status = '';
-
-  var _mergeFileList = [];
-
-  var _successNum = 0;
-
-  var _threadNum = 6;
-
-  var _progressFn = noop;
-
-  var _completeFn = noop;
-
-  var _totalNum = 0;
-
-  function startThread(num) {
-  	_status = 'start';
-  	for (var i = 0; i < num; i ++) {
-  		upload();
-  	};
-  }
-
-  function initFileSlice() {
-  	if (_fileList.length === 0) {
-  		_status = 'finsh';
-  		return ;
-  	};
-  	_currentFile = _fileList.pop();
-  	console.log(_currentFile)
-  	_currentSliceList = fileSlice(_currentFile, 1024*1024*4);
-    _totalNum = _currentSliceList.length;
   };
 
-  function mergeFile() {
-    if (_mergeFileList.indexOf(_currentFile.name) > -1) {
-      return;
+  Fileupload.fileSign = function (fileSign) {
+    _fileSign_ = fileSign;
+  };
+
+  function Fileupload() {
+
+    var _fileList = [];
+
+    var _currentSliceList = [];
+
+    var _uploadFileUrl = '';
+
+    var _queryFileUrl = '';
+
+    var _mergeFileUrl = '';
+
+    var _threadId = 0;
+
+    var _currentFile = '';
+
+    var _status = '';
+
+    var _mergeFileList = [];
+
+    var _successNum = 0;
+
+    var _threadNum = 6;
+
+    var _progressFn = noop;
+
+    var _completeFn = noop;
+
+    var _totalNum = 0;
+
+    function startThread(num) {
+      _status = 'start';
+      for (var i = 0; i < num; i++) {
+        upload();
+      };
+    }
+
+    function initFileSlice() {
+      if (_fileList.length === 0) {
+        _status = 'finsh';
+        return;
+      };
+      _currentFile = _fileList.pop();
+      console.log(_currentFile)
+      _currentSliceList = fileSlice(_currentFile, 1024 * 1024 * 4);
+      _totalNum = _currentSliceList.length;
     };
-    _mergeFileList.push(_currentFile.name);
-  	ajax({
-  		url: _mergeFileUrl,
-  		type: 'POST',
-  		data: {
-  			name: _currentFile.name
-  		},
-  		success : function(data) {
-        console.log(_currentFile.name + '合并成功');
-        _completeFn(_currentFile.name);
-  		},
-  		fail: function (e) {
-  		
-  		}
-  	});
-  };
 
-  function queryFile(md5, fn) {
-    ajax({
-      url: _queryFileUrl,
-      type: 'POST',
-      dataType: "json",
-      data: {
-        md5: md5
-      },
-      success : function(data) {
-        fn(data);
-      },
-      fail: function (e) {
-      
-      }
-    });
-  };
-  
-  function upload() {
-  	_status = 'running';
-  	_threadId++;
-  	console.log('线程' + _threadId + '开始');
+    function mergeFile() {
+      if (_mergeFileList.indexOf(_currentFile.name) > -1) {
+        return;
+      };
+      _mergeFileList.push(_currentFile.name);
+      ajax({
+        url: _mergeFileUrl,
+        type: 'POST',
+        data: {
+          name: _currentFile.name
+        },
+        success: function (data) {
+          console.log(_currentFile.name + '合并成功');
+          _completeFn(_currentFile.name);
+        },
+        error: function (e) {
 
-     var progress = 1- (_currentSliceList.length / _totalNum);
-    _progressFn.call(this, progress);
-  	// 完成
-  	if (_currentSliceList.length == 0) {
-      _successNum++;
-      if (_successNum === _threadNum) {
-        mergeFile();
-      }
-      return;
-  	};
-
-  	var temp = _currentSliceList.pop();
-  	var formData = new FormData();
-  	formData.append('file', temp.blob);
-    (function(blobTemp) {
-      getMd5(blobTemp.blob).then(function(md5) {
-        queryFile(md5, function(data) {
-          if (data == 1) {
-            console.log('相同');
-            upload();
-          } else {
-            formData.append('filename', _currentFile.name + '-' + blobTemp.index + '-' + md5);
-            ajax({
-              url: _uploadFileUrl,
-              type: 'POST',
-              cache: false,
-              data: formData,
-              processData: false,
-              contentType: false,
-              success: function(data) {
-                console.log('线程' + _threadId + '成功');
-                upload();
-              },
-              fail: function(e) {
-                _currentSliceList.push(blobTemp);
-                console.log('线程' + _threadId + '失败');
-                upload();
-              }
-            });
-          }
-        });
+        }
       });
-    })(temp);
-  }
+    };
 
-  this.init = function (option) {
-    assert(isObject(option), 'init only accept object');
-    assert(isString(option['upload']), 'init option\'s props \'upload\' must be string');
-    assert(isString(option['query']), 'init option\'s props \'query\' must be string');
-    assert(isString(option['merge']), 'init option\'s props \'merge\' must be string');
-    assert(isFunction(option['progress']), 'init option\'s props \'progress\' must be function');
-    _uploadFileUrl = option['upload'];
-    _queryFileUrl = option['query'];
-    _mergeFileUrl = option['merge'];
-    _progressFn = option['progress'];
-    _completeFn = option['complete'];
+    function queryFile(md5, fn) {
+      ajax({
+        url: _queryFileUrl,
+        type: 'POST',
+        dataType: "json",
+        data: {
+          md5: md5
+        },
+        success: function (data) {
+          fn(data);
+        },
+        error: function (e) {
+
+        }
+      });
+    };
+
+    function upload() {
+      _status = 'running';
+      _threadId++;
+      console.log('线程' + _threadId + '开始');
+      var progress = 1 - (_currentSliceList.length / _totalNum);
+      _progressFn.call(this, progress);
+      // 完成
+      if (_currentSliceList.length == 0) {
+        _successNum++;
+        if (_successNum === _threadNum) {
+          mergeFile();
+        }
+        return;
+      };
+      var temp = _currentSliceList.pop();
+      var formData = new FormData();
+      formData.append('file', temp.blob);
+      (function (blobTemp) {
+        getMd5(blobTemp.blob, function (md5) {
+          queryFile(md5, function (data) {
+            if (data == 1) {
+              console.log('相同');
+              upload();
+            } else {
+              formData.append('filename', _currentFile.name + '-' + blobTemp.index + '-' + md5);
+              ajax({
+                url: _uploadFileUrl,
+                type: 'POST',
+                cache: false,
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (data) {
+                  console.log('线程' + _threadId + '成功');
+                  upload();
+                },
+                error: function (e) {
+                  _currentSliceList.push(blobTemp);
+                  console.log('线程' + _threadId + '失败');
+                  upload();
+                }
+              });
+            }
+          });
+        });
+      })(temp);
+    }
+
+    this.init = function (option) {
+      assert(isObject(option), 'init only accept object');
+      assert(isString(option['upload']), 'init option\'s props \'upload\' must be string');
+      assert(isString(option['query']), 'init option\'s props \'query\' must be string');
+      assert(isString(option['merge']), 'init option\'s props \'merge\' must be string');
+      assert(isFunction(option['progress']), 'init option\'s props \'progress\' must be function');
+      _uploadFileUrl = option['upload'];
+      _queryFileUrl = option['query'];
+      _mergeFileUrl = option['merge'];
+      _progressFn = option['progress'];
+      _completeFn = option['complete'];
+    };
+
+    this.addFile = function (fileArr) {
+      _fileList = slice(fileArr);
+    };
+
+    this.upload = function () {
+      initFileSlice();
+      startThread(_threadNum);
+    };
   };
 
-  this.addFile = function (fileArr) {
-    _fileList = slice(fileArr);
-  };
+  Fileupload.prototype.version = version;
 
-  this.upload = function () {
-  	initFileSlice();
-    startThread(_threadNum);
-  };
-};
-
-Fileupload.prototype.version = version;
-
-return Fileupload;
+  return Fileupload;
 
 });
